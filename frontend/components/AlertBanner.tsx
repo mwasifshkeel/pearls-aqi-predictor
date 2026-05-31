a9bf4c12ccfa49bd0ce7dd2c6f2c10a25464782c
Muhammad Wasif Shakeel <mshakeel.bsds23seecs@seecs.edu.pk>
2026-05-31 19:27:27 +0500

    fix: add comment to clarify purpose of AlertBanner component

--- frontend/components/AlertBanner.tsx
+++ frontend/components/AlertBanner.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+// A simple banner to show important messages to the user. It can be used for things like maintenance notifications, new features, etc.
 export default function AlertBanner({ message }: { message: string }) {
   return <div className="banner">{message}</div>;
 }
